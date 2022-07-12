d939b2fe8cdc968bff69962aa07ce17ca9336452
OksanaOlefirenko <oksana_olefirenko@ukr.net>
2022-07-12 13:27:48 +0300

    add formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,74 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Label, Input, Button } from './ContactForm.styled';
+import { FormBox, Label, Input, Button, ErrorText } from './ContactForm.styled';
+import { Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { nanoid } from 'nanoid';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+const schema = yup.object().shape({
+  name: yup.string().min(2, 'Too Short!').max(30, 'Too Long!').required(),
+  number: yup.string().min(3).max(16).required(),
+});
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+const FormError = ({ name }) => {
+  return (
+    <ErrorMessage
+      name={name}
+      render={message => <ErrorText>{message}</ErrorText>}
+    />
+  );
+};
+
+export const ContactForm = ({ onSubmit }) => {
+  const nameId = nanoid();
+  const numberId = nanoid();
+
+  const handleSubmit = (value, { resetForm }) => {
+    onSubmit(value);
+    resetForm();
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label htmlFor={this.nameId}>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <FormBox>
+        <Label htmlFor={nameId}>
           Name
           <Input
             type="text"
             name="name"
+            id={nameId}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            value={name}
-            onChange={this.handleChange}
             required
           />
+          <FormError FormError name="name" />
         </Label>
 
-        <Label htmlFor={this.numberId}>
+        <Label htmlFor={numberId}>
           Number
           <Input
             type="tel"
             name="number"
+            id={numberId}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            value={number}
-            onChange={this.handleChange}
             required
           />
+          <FormError FormError name="number" />
         </Label>
         <Button type="submit">Add contact</Button>
-      </Form>
-    );
-  }
-}
+      </FormBox>
+    </Formik>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import { Form, Field } from 'formik';
 
-export const Form = styled.form`
+export const FormBox = styled(Form)`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   max-width: 400px;
   border-radius: 5px;
   padding: 10px;
@@ -10,20 +14,32 @@ export const Form = styled.form`
 export const Label = styled.label`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   margin-bottom: 20px;
   color: white;
 `;
 
-export const Input = styled.input`
+export const Input = styled(Field)`
   width: 200px;
-  margin-top: 2px;
+  height: 25px;
+  margin-top: 10px;
+  border: 1px solid #030e1845;
+  border-radius: 3px;
+  box-shadow: 0 0 3px 1px;
 `;
 
 export const Button = styled.button`
   cursor: pointer;
   border-radius: 5px;
+  width: 120px;
+  height: 30px;
   :hover {
-    background-color: yellowgreen;
+    background-color: teal;
     color: white;
   }
+`;
+
+export const ErrorText = styled.p`
+  color: red;
 `;
